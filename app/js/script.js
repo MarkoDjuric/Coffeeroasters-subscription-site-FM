917ef0dfd23518e7c23fcc3944d558db48fa8ab3
Marko djuric <maximumspid@gmail.com>
2022-02-10 00:39:08 +0100

    scss

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,26 @@
-console.log('HELLO');
+console.log('HELLO cofeeeeee');
 
 const test = () => {
 	console.log('this is a test');
 };
+
+let iconClose = "assets/shared/mobile/icon-close.svg";
+let iconOpen = "assets/shared/mobile/icon-hamburger.svg";
+
+const hamburger = document.getElementById("closed");
+
+
+
+
+document.addEventListener('click', () => {
+
+	if(hamburger.src.endsWith('icon-hamburger.svg') == true) {
+		
+		hamburger.src = iconClose;
+
+	}else {
+
+		hamburger.src= iconOpen;
+	}
+    console.log(hamburger.src)
+});
