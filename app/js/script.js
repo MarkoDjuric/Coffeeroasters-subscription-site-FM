0c78cd3dd18d812dc6486241e55db4295380671c
Marko djuric <maximumspid@gmail.com>
2022-02-24 01:16:25 +0100

    page links

--- app/js/script.js
+++ app/js/script.js
@@ -1,28 +1,46 @@
 
-	// Removeing video background
-	let heroDiv = document.getElementsByClassName('header__hero')[0];
+
+	// Removing video background
+	// let heroDiv;
+	let iconClose = "assets/shared/mobile/icon-close.svg";
+    let iconOpen = "assets/shared/mobile/icon-hamburger.svg";
+	const hamburger = document.getElementById("closed");
+	const minmenu = document.getElementById("minmenu");
+	let logo = document.getElementById("logo");
 	let video = document.getElementsByTagName('video')[0];
+
+
+if (video === undefined) {
+	console.log(true);
+	let heroDiv = document.getElementsByClassName('header__about')[0];
+	heroDiv.style.position = "unset";
+
+    document.getElementById('closed').addEventListener('click', () => {
+
+		if (hamburger.src.endsWith('icon-hamburger.svg') == true) {
+	
+			hamburger.src = iconClose;
+			hamburger.classList.add("shadow-text");
+			logo.classList.add("shadow-text");
+			minmenu.style.display = 'block';
+	
+		} else {
+			console.log(false);
+			hamburger.src = iconOpen;
+			hamburger.classList.remove("shadow-text");
+			logo.classList.remove("shadow-text");
+			minmenu.style.display = 'none';
+		}
+	
+	});
+
+}else {
+	
+	let heroDiv = document.getElementsByClassName('header__hero')[0];
 
 	video.onended = function (e) {
 		video.style.display = "none";
 		heroDiv.style.position = "unset";
-	};
-
-
-
-
-
-let iconClose = "assets/shared/mobile/icon-close.svg";
-let iconOpen = "assets/shared/mobile/icon-hamburger.svg";
-
-const hamburger = document.getElementById("closed");
-const minmenu = document.getElementById("minmenu");
-
-let logo = document.getElementById("logo");
-
-
-
-
 document.getElementById('closed').addEventListener('click', () => {
 
 	if (hamburger.src.endsWith('icon-hamburger.svg') == true) {
@@ -41,3 +59,93 @@ document.getElementById('closed').addEventListener('click', () => {
 	}
 
 });
+};
+}	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// Removeing video background
+	// let heroDiv = document.getElementsByClassName('header__hero')[0];
+	// let video = document.getElementsByTagName('video')[0];
+
+	// video.onended = function (e) {
+	// 	video.style.display = "none";
+	// 	heroDiv.style.position = "unset";
+
+		
+// let iconClose = "assets/shared/mobile/icon-close.svg";
+// let iconOpen = "assets/shared/mobile/icon-hamburger.svg";
+
+// const hamburger = document.getElementById("closed");
+// const minmenu = document.getElementById("minmenu");
+
+// let logo = document.getElementById("logo");
+
+
+
+
+// document.getElementById('closed').addEventListener('click', () => {
+
+// 	if (hamburger.src.endsWith('icon-hamburger.svg') == true) {
+
+// 		hamburger.src = iconClose;
+// 		hamburger.classList.add("shadow-text");
+// 		logo.classList.add("shadow-text");
+// 		minmenu.style.display = 'block';
+
+// 	} else {
+
+// 		hamburger.src = iconOpen;
+// 		hamburger.classList.remove("shadow-text");
+// 		logo.classList.remove("shadow-text");
+// 		minmenu.style.display = 'none';
+// 	}
+
+// });
+// 	};
+
+
+
+
